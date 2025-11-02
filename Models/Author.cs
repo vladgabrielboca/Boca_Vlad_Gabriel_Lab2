bf94aa35159bff9c2623b97bfbb50fcc7d9e73d0
vladgabrielboca <vladgabrielboca@gmail.com>
2025-11-02 12:07:55 +0200

    lab4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,9 +1,22 @@
-﻿namespace Boca_Vlad_Gabriel_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Boca_Vlad_Gabriel_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        public ICollection<Book>? Books { get; set; }
     }
 }
---
+++ Models/Viewmodels/PublisherIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Boca_Vlad_Gabriel_Lab2.Models.Viewmodels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
